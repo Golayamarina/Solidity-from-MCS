24d850de88cc33edf99c4f30337ec36d83c1b73a
Golayamarina <98704212+Golayamarina@users.noreply.github.com>
2022-09-24 12:08:49 +0300

    Create IERC721.sol

---
+++ ERC721/IERC721.sol
@@ -0,0 +1,18 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+interface IERC721 {
+    event Transfer(address indexed from, address indexed to, uint indexed tokenId);
+    event Approval(address indexed owner, address indexed approved, uint indexed tokenId);
+    event ApprovalForAll(address indexed owner, address indexed operator, bool approved);
+
+    function balanceOff(address owner) external view returns(uint);
+    function ownerOf(uint tokenId) external view returns(address);
+    function safeTransferFrom(address from, address to, uint tokenId, bytes calldata data) external;
+    function safeTransferFrom(address from, address to, uint tokenId) external;
+    function TransferFrom(address from, address to, uint tokenId) external;
+    function approve(address to, uint tokenId) external;
+    function setApproveForAll(address operator, bool approved) external;
+    function getApproved(uint tokenId) external view returns(address);
+    function isApprovedForAll(address owner, address operator) external;
+}
